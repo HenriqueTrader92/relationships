9cc30778a99dbf59c3cc2484a1c4f8566adb4005
= <=>
2019-03-07 20:34:45 -0300

    Aula 7 - Relacionamento um para muitos

--- app/Http/Controllers/OneToManyController.php
+++ app/Http/Controllers/OneToManyController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Country;
+use App\Models\State;
 
 class OneToManyController extends Controller
 {
     public function oneToMany()
     {
-        
+        // $country = Country::where('name', 'Brasil')->get()->first();
+        $keySearch = 'a';
+        $countries = Country::where('name', 'LIKE', "%{$keySearch}%")->get();
+
+        foreach($countries as $country){
+
+            echo "<b>{$country->name}</b>";
+
+            $states = $country->states;
+
+            foreach($states as $state){
+                echo"<br>{$state->initials} - {$state->name}";
+            }
+
+            echo '<hr>';
+        }
     }
+
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -8,8 +8,15 @@ class Country extends Model
 {
     protected $fillable = ['name'];
 
+    // Comunicação de UM para UM
     public function location()
     {
         return $this->hasOne(location::class);
+    }
+   
+    // Comunicação de UM para Muitos
+    public function states()
+    {
+        return $this->hasMany(State::class);
     }
 }
