c3ca04fa3073fbb741a531ee02205ad9d8bafdd6
= <=>
2019-03-06 22:13:55 -0300

    Aula 3 - Primeiro relacionamento One to One

---
+++ app/Http/Controllers/oneToOneController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Country;
+use App\Models\Location;
+
+class oneToOneController extends Controller
+{
+    public function oneToOne()
+    {
+        $country = Country::find(1);
+
+        echo $country->name;
+
+        $location = $country->location;
+        echo "<hr>Latitude: {$location->latitude}<br>";
+        echo "Longitude: {$location->longitude}<br>";
+    }
+}
